2d8797b7fdfdd49885c0103ce463e479e95e862a
Manfrin92 <44104737+Manfrin92@users.noreply.github.com>
2019-06-24 19:02:55 -0300

    Update monte_carlo_pi.py

--- monte_carlo_pi.py
+++ monte_carlo_pi.py
@@ -5,7 +5,7 @@ caiu_dentro = 0
 caiu_fora = 0
 picalculado = float()
 piReal = 3.141
-testes = 500000
+testes = 50000
 
 for x in range(0,testes):
     xAleatorio = uf(-0.5, 0.5)
@@ -30,5 +30,3 @@ print('pi calculado foi ' + str(picalculado))
 print('\n')
 print('erro de ' + str(picalculado-piReal))
 
-print(testes)
-print(caiu_dentro+caiu_fora)
